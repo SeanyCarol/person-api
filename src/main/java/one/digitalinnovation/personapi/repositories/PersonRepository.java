bdb2b9ae5169ec89605ab176646b7523b4577370
SeanyCarol <seany@ice.ufjf.br>
2021-07-06 07:09:39 -0300

    Creates first structure for creating person

---
+++ src/main/java/one/digitalinnovation/personapi/repositories/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.personapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import one.digitalinnovation.personapi.entities.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
