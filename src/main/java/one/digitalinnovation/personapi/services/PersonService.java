7b3f0194bf7b192b4a0bc448275aa623d9dddc68
SeanyCarol <seany@ice.ufjf.br>
2021-07-06 21:15:47 -0300

    Method to list all people developed

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -1,33 +1,43 @@
 package one.digitalinnovation.personapi.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
-  private PersonRepository personRepository;
+    private PersonRepository personRepository;
 
-  private final PersonMapper personMapper = PersonMapper.INSTANCE;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-  @Autowired
-  public PersonService(PersonRepository personRepository) {
-      this.personRepository = personRepository;
-  }
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
-  public MessageResponseDTO createPerson(PersonDTO personDTO) {
-    Person personToSave = personMapper.toModel(personDTO);
-    Person savedPerson = personRepository.save(personToSave);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
 
-    return MessageResponseDTO
-            .builder()
-            .message("Created person with ID " + savedPerson.getId())
-            .build();
-  }
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
